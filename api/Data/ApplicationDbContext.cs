f87fb1749698f584483b63cd8b8c478a8f1b25ba
phammings <ryanlepham9@gmail.com>
2025-12-02 21:14:16 -0500

    feat: initialized models

--- api/Data/ApplicationDbContext.cs
+++ api/Data/ApplicationDbContext.cs
@@ -4,10 +4,15 @@ using BoatAdminApi.Models;
 namespace BoatAdminApi.Data
 {
     public class ApplicationDbContext : DbContext
-    {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-            : base(options) { }
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
 
-        public DbSet<Boat> Boats { get; set; }
-    }
+    public DbSet<BoatSale> BoatSales { get; set; }
+    public DbSet<BoatPhoto> BoatPhotos { get; set; }
+    public DbSet<BoatVideo> BoatVideos { get; set; }
+    public DbSet<VehicleCategory> VehicleCategories { get; set; }
+    public DbSet<VehicleClass> VehicleClasses { get; set; }
+    public DbSet<City> Cities { get; set; }
+}
+
 }
