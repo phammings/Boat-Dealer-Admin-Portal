7759b3cf375a9149fb68b7c76b37aeb5bda0e642
phammings <ryanlepham9@gmail.com>
2025-12-26 14:43:25 -0500

    feat: implements toast success

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,10 +2,12 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import BoatsPage from "./pages/BoatsPage";
 import BoatViewPage from "./pages/BoatViewPage";
 import BoatCreatePage from "./pages/BoatCreatePage";
+import { ToastContainer } from "react-toastify";
 
 export default function App() {
   return (
     <BrowserRouter>
+      <ToastContainer />
       <Routes>
         <Route path="/" element={<BoatsPage />} />
         <Route path="/view/:id" element={<BoatViewPage />} />
